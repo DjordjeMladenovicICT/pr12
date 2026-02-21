61cf88f8f53a99c310888b2362611987826088d0
DjordjeMladenovicICT <142057305+DjordjeMladenovicICT@users.noreply.github.com>
2026-02-21 22:11:52 +0100

    Update program.c
    Ispis povrsine

--- program.c
+++ program.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-int a,b;
+int a,b,p;
 printf("Unesi vrednosti\n");
 scanf_s("%d%d",&a,&b);
 
 p=a*b;
-printf("");
+printf("Ispis rezultata je ",p);
 return 0;
 }
