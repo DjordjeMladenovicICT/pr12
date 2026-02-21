3e651ca3126e220bbbe9ace3ed3b0afe309bcaa2
DjordjeMladenovicICT <djordje.mladenovic@ict.edu.rs>
2026-02-21 22:39:07 +0100

    Dodatak

--- program.c
+++ program.c
@@ -6,6 +6,8 @@ printf("Unesi vrednosti\n");
 scanf_s("%d%d",&a,&b);
 
 p=a*b;
-printf("Ispis rezultata je ",p);
+printf("Ispis rezultata je %d",p);
+printf("Ispis rezultata je %d",p);
+printf("Ispis rezultata je %d",p);
 return 0;
 }
