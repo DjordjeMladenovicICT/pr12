35b2c0be1aa9f259af0b6154b63dc7ceb386056e
DjordjeMladenovicICT <djordje.mladenovic@ict.edu.rs>
2026-03-09 12:24:07 +0100

    Iymena je u redu

--- program.c
+++ program.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-int a,b,p;
 printf("Unesi vrednosti\n");
 scanf_s("%d%d",&a,&b);
 
 p=a*b;
+d=a=b;
 printf("Ispis rezultata je %d",p);
 printf("Ispis rezultata je %d",p);
-printf("Ispis rezultata je %d",p);
+
 
 return 0;
 }
